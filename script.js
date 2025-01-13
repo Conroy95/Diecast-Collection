2d1365149ed5900b03eba1bfa4ecfdbeaa5fc1e8
Conroy95 <leandervdneut@ziggo.nl>
2025-01-13 18:35:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 // Array met verzamelde miniatuur auto's
 const autoData = [
     {
+        logo: "https://www.carlogos.org/car-logos/ferrari-logo-2002-640.png",
         merk: "Ferrari",
         model: "F40",
         jaar: "1987",
         schaal: "1:18",
         categorie: "Sportwagen",
-        opmerking: "Uitgebracht ter gelegenheid van het 40-jarig jubileum.",
-        logo: "https://www.carlogos.org/car-logos/ferrari-logo-2002-640.png"
+        opmerking: "Uitgebracht ter gelegenheid van het 40-jarig jubileum."
     },
     {
+
+        logo: "https://www.carlogos.org/car-logos/porsche-logo-2014-full-640.png",        
         merk: "Porsche",
         model: "911",
         jaar: "1965",
         schaal: "1:24",
         categorie: "Sportwagen",
-        opmerking: "De iconische Porsche 911.",
-        logo: "https://www.carlogos.org/car-logos/porsche-logo-2014-full-640.png"
+        opmerking: "De iconische Porsche 911."
     },
     {
+        logo: "https://www.carlogos.org/logo/Volkswagen-logo-2019-640x500.jpg",
         merk: "Volkswagen",
         model: "Beetle",
         jaar: "1968",
         schaal: "1:32",
         categorie: "Volkswagen",
-        opmerking: "De klassieke Volkswagen Kever.",
-        logo: "https://www.carlogos.org/logo/Volkswagen-logo-2019-640x500.jpg"
+        opmerking: "De klassieke Volkswagen Kever."
     }
 ];
 
